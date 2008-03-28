456cd97a27de3f2ec6441d4206fad52b07ad0ecf
Jonathan Rockway <jon@jrock.us>
2008-03-28 06:23:48 -0500

    improve repl

--- emacs/stylish-repl.el
+++ emacs/stylish-repl.el
@@ -4,9 +4,12 @@
 
 (defun stylish-handler-repl (status result)
   "Handle a return from the REPL"
-  (insert result)
-  (insert "\n")
-  (insert-stylish-repl-prompt))
+  (with-current-buffer (get-buffer "*Stylish REPL*")
+    (goto-char (point-max))
+    (insert result)
+    (insert "\n")
+    (insert-stylish-repl-prompt)
+    (goto-char (point-max))))
 
 (defun stylish-repl-send nil
   "Send a command to the REPL"
@@ -16,7 +19,16 @@
     (let ((command (buffer-substring-no-properties 
                     (match-end 0)
                     (save-excursion (end-of-line) (point)))))
-      (stylish-send-command 'repl command))))
+      (stylish-send-command 'repl command)))
+  (insert "\n"))
+
+(defun stylish-repl-send-file (&optional buffer)
+  (interactive)
+  (or buffer (setq buffer (current-buffer)))
+  (let ((fn (buffer-file-name buffer)))
+    (stylish-send-command 'repl-load-file fn)
+    (with-current-buffer (get-buffer "*Stylish REPL*")
+      (insert (format "\n# Sending %s\n" fn)))))
 
 (defun stylish-repl nil
   (interactive)
@@ -27,8 +39,9 @@
   "The major mode for the Stylish REPL buffer."
   (stylish)
   (insert "Welcome to the Stylish REPL!\n")
+  (message "Let the hacking commence!")
   (insert-stylish-repl-prompt)
-  (local-set-key (kbd "<enter>") 'stylish-repl-send))
+  (local-set-key (kbd "C-c C-c") 'stylish-repl-send))
 
 (defun insert-stylish-repl-prompt nil
   (insert (propertize "PERL>" 'face font-lock-keyword-face))
